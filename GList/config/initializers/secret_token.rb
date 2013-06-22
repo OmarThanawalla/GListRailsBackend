834e1cd39ab8b7db42fe8f633d7986e30aff0cbd
Omar Thanawalla <omar.thanawalla@gmail.com>
2013-06-22 11:17:03 -0500

    initial commit

---
+++ GList/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GList::Application.config.secret_token = 'e83afc89390724dada07906fcd836edcab5197263bdd6558a8b30d5d876394031f0dbaa3de3f87145647f1f57d77ddb94914c660767f561298bd1f102b4dfb17'
